e366c143fb9cfa05b5a72a19713902fa93da49d4
LegspCpd <3572653147@qq.com>
2026-04-03 03:31:33 +0800

    Update status page title and links

--- uptime.config.ts
+++ uptime.config.ts
@@ -7,12 +7,12 @@ import { MaintenanceConfig, PageConfig, WorkerConfig } from './types/config'
 
 const pageConfig: PageConfig = {
   // Title for your status page
-  title: "lyc8503's Status Page",
+  title: "LegspCpd's Status Page",
   // Links shown at the header of your status page, could set `highlight` to `true`
   links: [
-    { link: 'https://github.com/lyc8503', label: 'GitHub' },
-    { link: 'https://blog.lyc8503.net/', label: 'Blog' },
-    { link: 'mailto:me@lyc8503.net', label: 'Email Me', highlight: true },
+    { link: 'https://github.com/legspCpd846', label: 'GitHub' },
+    { link: 'https://blog.2xxt.qzz.io/', label: 'Blog' },
+    { link: 'mailto:me@legspcpd.qzz.io', label: 'Email Me', highlight: true },
   ],
 }
 
