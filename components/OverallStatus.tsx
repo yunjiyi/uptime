11f009e2900a4989312cd3f81500839ad3244cf6
yunjiyi <135610869+yunjiyi@users.noreply.github.com>
2026-04-07 06:48:24 -0700

    Update OverallStatus.tsx

--- components/OverallStatus.tsx
+++ components/OverallStatus.tsx
@@ -37,7 +37,81 @@ export default function OverallStatus({
     statusString = t('All systems not operational')
   } else if (state.overallDown === 0) {
     statusString = t('All systems operational')
-    icon = <IconCircleCheck style={{ width: 64, height: 64, color: '#059669' }} />
+//  icon = <IconCircleCheck style={{ width: 64, height: 64, color: '#059669' }} />
+    icon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="64"
+    height="64"
+    viewBox="0 0 24 24"
+  >
+    <circle cx="12" cy="12" r="0" fill="#129900">
+      <animate
+        id="svgSpinnersPulseMultiple0"
+        fill="freeze"
+        attributeName="r"
+        begin="0;svgSpinnersPulseMultiple2.end"
+        calcMode="spline"
+        dur="1.2s"
+        keySplines=".52,.6,.25,.99"
+        values="0;11"
+      />
+      <animate
+        fill="freeze"
+        attributeName="opacity"
+        begin="0;svgSpinnersPulseMultiple2.end"
+        calcMode="spline"
+        dur="1.2s"
+        keySplines=".52,.6,.25,.99"
+        values="1;0"
+      />
+    </circle>
+    <circle cx="12" cy="12" r="0" fill="#129900">
+      <animate
+        id="svgSpinnersPulseMultiple1"
+        fill="freeze"
+        attributeName="r"
+        begin="svgSpinnersPulseMultiple0.begin+0.2s"
+        calcMode="spline"
+        dur="1.2s"
+        keySplines=".52,.6,.25,.99"
+        values="0;11"
+      />
+      <animate
+        fill="freeze"
+        attributeName="opacity"
+        begin="svgSpinnersPulseMultiple0.begin+0.2s"
+        calcMode="spline"
+        dur="1.2s"
+        keySplines=".52,.6,.25,.99"
+        values="1;0"
+      />
+    </circle>
+    <circle cx="12" cy="12" r="0" fill="#129900">
+      <animate
+        id="svgSpinnersPulseMultiple2"
+        fill="freeze"
+        attributeName="r"
+        begin="svgSpinnersPulseMultiple0.begin+0.4s"
+        calcMode="spline"
+        dur="1.2s"
+        keySplines=".52,.6,.25,.99"
+        values="0;11"
+      />
+      <animate
+        fill="freeze"
+        attributeName="opacity"
+        begin="svgSpinnersPulseMultiple0.begin+0.4s"
+        calcMode="spline"
+        dur="1.2s"
+        keySplines=".52,.6,.25,.99"
+        values="1;0"
+      />
+    </circle>
+  </svg>
+)
+
+    
   } else {
     statusString = t('Some systems not operational', {
       down: state.overallDown,
